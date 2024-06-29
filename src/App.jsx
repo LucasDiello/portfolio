9cd9f08ace6caf901fd262e6d1a0207aca84c545
Lucas <lucasoliveiradiello@hotmail.com>
2024-06-29 20:13:29 -0300

    Feat: implement about in home

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import About from './routes/About'
 import Perfil from './routes/Perfil'
 
 function App() {
   return (
     <div className='App'>
         <Perfil />
+        <About />
       </div>
   )
 }
---
+++ src/routes/About.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import "../styles/animation.css"
+import "../styles/about.css"
+
+export default function About() {
+    return (
+            <section class="about-me">
+                <div class="heading-text hidd show">
+                    <h2 class="heading-5-a">Sobre mim</h2>
+                    <div class="about-me-text">
+                        <p class="para-5-a leading-5">Tenho 21 anos e sou desenvolvedor Full Stack com enfâse em front end.
+                            Esta posição me permite explorar tanto o lado do front-end quanto do back-end. <br />
+                            Sou capaz de criar aplicações web completas, desde a interface do usuário até a lógica de
+                            negócios e o gerenciamento de bancos de dados. <br />
+                            Meu objetivo é sempre fornecer soluções eficazes e eficientes, que atendam às necessidades do cliente e superem
+                            suas expectativas. <br />
+                            Estou sempre em busca de novas oportunidades de aprendizado,
+                            e estou ansioso para ver o que o futuro  reserva para mim.  </p>
+                        </div>
+                        </div>
+                        </section>
+                    )
+}
